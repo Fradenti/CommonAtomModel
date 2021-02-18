e495a77c45b81643cc838cbcb6aad7c0ba9216a0
Francesco Denti <fdenti@uci.edu>
2021-02-17 21:41:08 -0800

    Update Plotresults_Competitor.R

--- Competitors/Plotresults_Competitor.R
+++ Competitors/Plotresults_Competitor.R
@@ -59,3 +59,57 @@ ggsave("Competitors/Jags_ARINFD_DC_jags_S2.pdf",width = 12,height = 6)
 
 ggsave("Competitors/All_ARINFD_DC_jags_S2.png",width = 12,height = 8)
 ggsave("Competitors/All_ARINFD_DC_jags_S2.pdf",width = 12,height = 8)
+
+
+
+
+# tabluar report ----------------------------------------------------------
+
+c1 <- map(tj1a_c, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  )) 
+n1 <- map(tj1a_n, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  )) 
+h1 <- map(tj1a_h, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  )) 
+c2 <- map( tj2_c, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  ))  
+n2 <- map( tj2_n, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  ))  
+h2 <- map( tj2_h, ~ apply(.x,2,function(x)  c(mean(x),sd(x))  ))  
+
+
+ARI <- rbind(cbind(c1$ARI,
+                   n1$ARI,
+                   h1$ARI),
+                   cbind(c2$ARI,
+                   n2$ARI,
+                   h2$ARI))
+
+NFD <- rbind(cbind(c1$NFD,
+                   n1$NFD,
+                   h1$NFD),
+             cbind(c2$NFD,
+                   n2$NFD,
+                   h2$NFD))
+
+round(rbind(ARI,NFD),4)
+X <- as_tibble(t(round(rbind(ARI,NFD),4))) %>% 
+  mutate(V2 = paste0("(",V2,")"),
+         V4 = paste0("(",V4,")"),
+         V6 = paste0("(",V6,")"),
+         V8 = paste0("(",V8,")"))
+t(X)
+
+
+CLU <- rbind(cbind(c1$numclust,
+                    n1$numclust,
+                    h1$numclust),
+              cbind(c2$numclust,
+                    n2$numclust,
+                    h2$numclust))
+CLU
+
+
+
+
+c1 <- apply(tj1a_c[[1]],2,function(x)  names(table(x))[which.max(table(x))]  ) 
+n1 <- apply(tj1a_n[[1]],2,function(x)  names(table(x))[which.max(table(x))]  ) 
+h1 <- apply(tj1a_h[[1]],2,function(x)  names(table(x))[which.max(table(x))]  ) 
+c2 <- apply( tj2_c[[1]],2,function(x)  names(table(x))[which.max(table(x))]  )  
+n2 <- apply( tj2_n[[1]],2,function(x)  names(table(x))[which.max(table(x))]  )  
+h2 <- apply( tj2_h[[1]],2,function(x)  names(table(x))[which.max(table(x))]  )  
